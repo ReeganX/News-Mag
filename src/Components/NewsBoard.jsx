7953588c9a8e9b8dca49db5c46f967c55519625b
ReeganX <ghoraninja641999@gmail.com>
2025-03-17 11:22:29 +0530

    Removed base path from vite.config.js

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -3,6 +3,7 @@ import NewsItem from "./NewsItem";
 
 const NewsBoard = ({category}) => {
     const [articles,setArticals] = useState([]);
+    
     useEffect(()=>{
          let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
          fetch(url).then(response=> response.json()).then(data=> setArticals(data.articles));
